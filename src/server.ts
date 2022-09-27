4f29a3d033863091905062e34a92733ee336f210
Uko, God'sgift Matthew <godsgiftuko@gmail.com>
2022-09-27 18:57:22 +0100

    Updated server.ts

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,8 @@ import { expressHttpAdapter } from "./adapters/express.adapter";
 // import { auth, requiresAuth } from "express-openid-connect";
 import { generateAccessToken, verifyAccessToken } from "./API/1.0/middlewares";
 
+
+console.log({APP_VAR})
 const httpServer = () => {
   const PORT = APP_VAR.serverPort;
   const apiPath = APP_VAR.apiPath; 
